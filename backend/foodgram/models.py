b328919cde7e1e0b2a4b9afba86602b8e679187f
Evgeniy Galeev <pioneer080@mail.ru>
2023-10-09 11:21:41 +0500

    abstract models

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Recipe(models.Model):
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.SET_NULL,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=100)
+    image = models.ImageField(
+        verbose_name='Картинка',
+        help_text='Загрузить картинку',
+    )
+    text = models.TextField(verbose_name='Описание')
+    ingredient = models.ManyToManyField(
+        Ingredient,
+        blank=False,
+        through='IngredientInRecipe',
+    )
+    cooking_time = models.PositiveSmallIntegerField()
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    color = models.CharField(max_length=7, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ('name',)
+
+    def __str__(self):
+        return f'{self.name}{self.color}'
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    measurement_unit = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ('name',)
+
+    def __str__(self):
+        return f'{self.name}{self.measurement_unit}'
+
+
+class IngredientInRecipe(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='ingredient',
+        on_delete=models.CASCADE,
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        related_name='recipe',
+        on_delete=models.CASCADE,
+    )
+    amount = models.PositiveSmallIntegerField()
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self) -> str:
+        return f"{self.amount}{self.ingredients}"
