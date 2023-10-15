2e3708c1474a536357596af5296781e009e8264d
Evgeniy Galeev <pioneer080@mail.ru>
2023-10-15 20:53:55 +0500

    fix models for migrations

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -4,36 +4,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class Recipe(models.Model):
-    tag = models.ForeignKey(
-        Tag,
-        on_delete=models.SET_NULL,
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
-    name = models.CharField(max_length=100)
-    image = models.ImageField(
-        verbose_name='Картинка',
-        help_text='Загрузить картинку',
-    )
-    text = models.TextField(verbose_name='Описание')
-    ingredient = models.ManyToManyField(
-        Ingredient,
-        blank=False,
-        through='IngredientInRecipe',
-    )
-    cooking_time = models.PositiveSmallIntegerField()
-
-    class Meta:
-        verbose_name = 'Рецепт'
-        verbose_name_plural = 'Рецепты'
-
-    def __str__(self):
-        return self.name
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=200, unique=True)
     color = models.CharField(max_length=7, unique=True)
@@ -61,13 +31,50 @@ class Ingredient(models.Model):
         return f'{self.name}{self.measurement_unit}'
 
 
+class Recipe(models.Model):
+    tag = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор рецепта',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    name = models.CharField(verbose_name='Название рецепта', max_length=100)
+    image = models.ImageField(
+        verbose_name='Картинка',
+        help_text='Загрузить картинку',
+    )
+    text = models.TextField(verbose_name='Описание')
+    ingredient = models.ManyToManyField(
+        Ingredient,
+        blank=False,
+        through='IngredientInRecipe',
+        related_name='recipes',
+    )
+    cooking_time = models.PositiveSmallIntegerField()
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
 class IngredientInRecipe(models.Model):
     recipe = models.ForeignKey(
         Recipe,
-        related_name='ingredient',
+        related_name='ingredients',
         on_delete=models.CASCADE,
     )
-    ingredient = models.ForeignKey(
+    ingredients = models.ForeignKey(
         Ingredient,
         related_name='recipe',
         on_delete=models.CASCADE,
@@ -82,14 +89,24 @@ class IngredientInRecipe(models.Model):
         return f"{self.amount}{self.ingredients}"
 
 class Favorite(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = ''
-        verbose_name_plural = ''
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранные'
 
     def __str__(self):
-        return f'{}{}'
+        return f'{self.user}{self.recipe}'
 
 
 class Cart(models.Model):
-    pass
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранные'
+
+    def __str__(self):
+        return f'{self.user}{self.recipe}'
