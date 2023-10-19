5205a880880fcc399d0e7bcc7be85b283065eac9
Evgeniy Galeev <pioneer080@mail.ru>
2023-10-19 11:10:45 +0500

    Add DefaultRouter

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from api.views import TagViewSet, IngredientViewSet
+
+router = DefaultRouter()
+
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
+    path('', include(router.urls)),
     # path('users/', )
 ]
