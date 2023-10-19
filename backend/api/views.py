f74055be623d8f2026058f528098f6e3a629840c
Evgeniy Galeev <pioneer080@mail.ru>
2023-10-19 11:12:41 +0500

    Add Tag, Ingredient ViewSet

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-# Create your views here.
+from api.serializers import TagSerializer, IngredientSerializer
+from foodgram.models import Tag, Ingredient, Recipe, Favorite, Cart
+
+User = get_user_model()
+
+
+class TagViewSet(ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class RecipeViewSet():
+    pass
+
+
+# class FavoriteViewSet():
+#     pass
+#
+#
+# class CartViewSet():
+#     pass
